4341f27b40f56bd30b09ac8943d0beb3913b9752
turpp <white.tripp@icloud.com>
2020-11-24 08:56:47 -0600

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -8,9 +8,26 @@ class StudentsController < ApplicationController
   def show
   end
 
+  def activate
+    set_student
+  case
+  when @student.active==true
+    # @student.active=(false)
+    @student.update(active: false)
+  when @student.active==false
+    # @student.active=(true)
+    @student.update(active: true)
+  else 
+    "error"
+  end
+  # redirect_to "/students/#{@student.id}"
+  redirect_to student_path(@student)
+  end
+
   private
 
     def set_student
       @student = Student.find(params[:id])
     end
+
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+    def status_student
+    
+            if @student.active== true
+                "active"
+            else
+                "inactive"
+            end            
+          
+    end
 end
